b014cdef2336965674efc3974595770cfa87762e
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2025-02-18 17:58:19 +0100

    feat(models): create models user and profile, custom admin site

--- salon/users/admin.py
+++ salon/users/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser, Employee, Profile
 
-admin.site.register(CustomUser)
-admin.site.register(Employee)
-admin.site.register(Profile)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = (
+        "email",
+        "username",
+        "phone_number",
+        "is_customer",
+        "is_employee",
+        "is_staff",
+    )
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Additional info:",
+            {"fields": ("phone_number", "is_customer", "is_employee")},
+        ),
+    )
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("user", "work_start", "work_end")
+    search_fields = ("user__username", "user__email")
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "image")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
