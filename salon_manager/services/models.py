a175c114f76a3bc96ffa0254f31868cd36509e2f
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2025-02-21 07:04:31 +0100

    feat(services): create app services and make model

---
+++ salon_manager/services/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class ServiceCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(
+        ServiceCategory, on_delete=models.CASCADE, related_name="services"
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    duration = models.PositiveIntegerField()
+
+    def __repr__(self):
+        return self.name
