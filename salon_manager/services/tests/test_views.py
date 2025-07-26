24fca9c4a15b525fa7f156f8c47f761fcf23c3fb
Pawel-Truszkowski <pawel.truszkowski14@gmail.com>
2025-07-26 07:02:46 +0200

    tests: testing manage service list view has been done

--- salon_manager/services/tests/test_views.py
+++ salon_manager/services/tests/test_views.py
@@ -10,23 +10,23 @@ class ServiceViewsTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
-        username = "testuser"
-        test_password = "testpassword123"  # nosec
-
-        self.user = CustomUser.objects.create_user(
-            username=username, password=test_password
+        self.owner_user = CustomUser.objects.create_user(
+            username="owner", password="test", role="OWNER"
+        )
+        self.other_user = CustomUser.objects.create_user(
+            username="not_owner", password="test"
         )
 
         self.category = ServiceCategory.objects.create(
-            name="Test", description="Test Category"
+            name="TestCategory", description="This is category test"
         )
         self.service = Service.objects.create(
-            name="Test", category=self.category, price=50.00, duration=30
+            name="TestServie", category=self.category, price=50.00, duration=30
         )
 
 
 class ServiceListViewTest(ServiceViewsTestCase):
-    def test_list_view_if_success(self):
+    def test_view_if_success(self):
         response = self.client.get(reverse("services_list"))
         self.assertEqual(response.status_code, 200)
 
@@ -45,4 +45,36 @@ class ServiceListViewTest(ServiceViewsTestCase):
     def test_view_with_no_services(self):
         Service.objects.all().delete()
         response = self.client.get(reverse("services_list"))
+        self.assertEqual(len(response.context["services"]), 0)
+
+
+class ManageServicesListViewTest(ServiceViewsTestCase):
+    def test_view_for_nobody_login(self):
+        response = self.client.get(reverse("manage_services_list"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_for_login_other_user(self):
+        self.client.login(username="not_owner", password="test")
+        response = self.client.get(reverse("manage_services_list"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_view_for_login_owner(self):
+        self.client.login(username="owner", password="test")
+        response = self.client.get(reverse("manage_services_list"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username="owner", password="test")
+        response = self.client.get(reverse("manage_services_list"))
+        self.assertTemplateUsed(response, "services/manage_services_list.html")
+
+    def test_view_for_services_in_context_data(self):
+        self.client.login(username="owner", password="test")
+        response = self.client.get(reverse("manage_services_list"))
+        self.assertIn(self.service, response.context["services"])
+
+    def test_view_with_no_data_in_context(self):
+        Service.objects.all().delete()
+        self.client.login(username="owner", password="test")
+        response = self.client.get(reverse("manage_services_list"))
         self.assertEqual(len(response.context["services"]), 0)
